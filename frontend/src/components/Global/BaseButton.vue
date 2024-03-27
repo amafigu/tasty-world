dd5062367ab6a2bc945d0a4634a69e9dc319db41
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-27 16:07:23 +0100

    provisory Home page and test api button

---
+++ frontend/src/components/Global/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="getRecipies">Get Recipes</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { getRecipies } from '../../services/api/getRecipies'
+
+const recipes = ref([])
+
+const fetchRecipes = async () => {
+  try {
+    const data = await getRecipies()
+    recipes.value = data
+    console.log(recipes.value)
+  } catch (err) {
+    console.error(err)
+  }
+}
+</script>
