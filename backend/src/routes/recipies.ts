8262506725291741477bad641528ea5660cc7e30
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-28 11:48:27 +0100

    add test recipies

--- backend/src/routes/recipies.ts
+++ backend/src/routes/recipies.ts
@@ -4,7 +4,29 @@ const router = Router()
 
 router.get('/', async (req: Request, res: Response) => {
     try {
-        return res.status(200).json({message: 'GET R'})
+        const recipies = [
+            {
+                id: 1,
+                imagePath: "meat.jpg",
+                title: 'barbecue',
+                subtitle: 'In someplace there is no better option than a barbecue....',
+                category: 'meat',
+                readyIn: '70 minutes',
+                recipeIngredients: 'meat, vegetables ... table ingredients',
+                cookingStepts: 'first: make fire, second: prepare the meat and the salad'
+            },{
+                id: 2,
+                imagePath: "pizza.jpg",
+                title: 'Pizza bianca',
+                subtitle: 'There are ocasion where everyone just loves pizza, why not to do that at home....',
+                category: 'vegeratian',
+                readyIn: '50 minutes',
+                recipeIngredients: 'flour, tomato souce ... ingredients',
+                cookingStepts: 'first: make dough, second: prepare the souce'
+            }
+
+        ]
+        return res.status(200).json(recipies)
     } catch(error) {
        return res.status(500).json({error: 'error by getting recipies'})
     }
