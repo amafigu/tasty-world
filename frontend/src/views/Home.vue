d3f8fd149ff3b8767fa607673ca3d4dae0add15e
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-28 13:06:38 +0100

    render Slider with backend fetched data

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,23 @@
-<script setup>
-import BaseButton from '@/components/Global/BaseButton.vue'
-</script>
-
 <template>
   <main>
-    <BaseButton></BaseButton>
+    <Slider :items="recipes"></Slider>
   </main>
 </template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import Slider from '@/components/Slider/Slider.vue'
+import { getRecipies } from '@/services/api/getRecipies'
+
+const recipes = ref([])
+
+onMounted(async () => {
+  try {
+    const recipesResponse = await getRecipies()
+    recipes.value = recipesResponse.data
+    console.log(recipesResponse)
+  } catch (error) {
+    console.error('Failed to get recipes:', error)
+  }
+})
+</script>
