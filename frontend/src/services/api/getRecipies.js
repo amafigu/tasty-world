25032926bd91aaacf4388b3cffd08671b609c899
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-03-28 13:00:47 +0100

    use env VITE_APP_API_DOMAIN

--- frontend/src/services/api/getRecipies.js
+++ frontend/src/services/api/getRecipies.js
@@ -2,10 +2,9 @@ import { axios } from '../axios/config'
 
 export const getRecipies = async () => {
   try {
-    const response = await axios.get(`${import.meta.env.VUE_APP_API_DOMAIN}/recipies`)
+    const response = await axios.get('/recipies')
     if (response && response.status === 200) {
-      console.log(response)
-      return response.data
+      return response
     }
   } catch (err) {
     console.error(err)
--- frontend/src/services/axios/config.js
+++ frontend/src/services/axios/config.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 const instance = Axios.create({
-  baseURL: import.meta.env.VUE_APP_API_DOMAIN,
+  baseURL: import.meta.env.VITE_APP_API_DOMAIN,
   responseType: 'json',
   timeout: 20000
 })
