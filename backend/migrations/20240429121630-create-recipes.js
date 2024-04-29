76ea15555515bb12e00cc42eb47b396a8f1a69cb
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-04-29 15:32:21 +0200

    add migration for recipes table

---
+++ backend/migrations/20240429121630-create-recipes.js
@@ -0,0 +1,55 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("recipes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      time: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      ingredients: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      instructions: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("recipes");
+  },
+};
