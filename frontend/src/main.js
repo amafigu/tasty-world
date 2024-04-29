b7803d0a3b1a029e188b3627f4c9e4bc61cc61f3
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-04-29 21:43:02 +0200

    config entrypoint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,29 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp, provide, h } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core'
+import fetch from 'cross-fetch'
 
-const app = createApp(App)
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+  fetch
+})
+
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache
+})
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+  render: () => h(App)
+})
 
 app.use(router)
 
