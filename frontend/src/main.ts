afb9e6f5d72807ba0bd622c483f864f3e830075c
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-18 16:39:29 +0200

    style: add global styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import router from './router'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client/core'
 import fetch from 'cross-fetch'
+import './assets/global.css'
 
 const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
