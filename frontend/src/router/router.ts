81f194e486c40fa94efce6372179c4ee43c99408
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-18 16:37:41 +0200

    refactor: update router

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -5,6 +5,26 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     name: 'home',
     component: () => import('../views/Home/Home.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/About/About.vue')
+  },
+  {
+    path: '/meat-recipies',
+    name: 'meat',
+    component: () => import('../views/recipes/Meat/Meat.vue')
+  },
+  {
+    path: '/fish-recipies',
+    name: 'fish',
+    component: () => import('../views/recipes/Fish/Fish.vue')
+  },
+  {
+    path: '/vegetarian-recipies',
+    name: 'vegetarian',
+    component: () => import('../views/recipes/Vegetarian/Vegetarian.vue')
   }
 ]
 
