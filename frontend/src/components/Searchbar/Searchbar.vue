a96f0a3cbf42eb04633a62ab0cf48bb98b8b6de1
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-18 16:38:54 +0200

    feat: add seachbar without functionality

---
+++ frontend/src/components/Searchbar/Searchbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="searchbar-container">
+    <input
+      type="text"
+      placeholder="search for a recipe, with ingredients or name"
+    />
+    <icon-mdi-magnify class="search-icon" />
+  </div>
+</template>
+
+<script setup>
+import IconMdiMagnify from "~icons/mdi/magnify";
+</script>
+
+<style scoped>
+.searchbar-container {
+  width: 30%;
+  display: flex;
+  align-items: center;
+
+  input {
+    height: 28px;
+    margin-right: 1rem;
+    width: 100%;
+    border-radius: var(--border-radius-small);
+  }
+
+  .search-icon {
+    height: 32px;
+    width: 32px;
+  }
+}
+</style>
