ff4d9119c61c01520b3feaa7eac2b5cf890b35c7
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-19 13:04:13 +0200

    feature: add Slider to Homeview

--- frontend/src/components/Slider/Slider.vue
+++ frontend/src/components/Slider/Slider.vue
@@ -1,26 +1,68 @@
 <template>
-  <div class="slider-container">
-    <div>
-      <div v-for="item in items" :key="item.id" class="flicking-panel">
-        <RecipeCard :recipe="item" />
-      </div>
-    </div>
+  <div>
+    <Carousel v-bind="settings" :breakpoints="breakpoints" class="slider">
+      <Slide class="slider-item" v-for="item in items" :key="item">
+        <SliderCard :recipe="item" />
+      </Slide>
+      <template #addons>
+        <Navigation />
+      </template>
+    </Carousel>
   </div>
 </template>
 
 <script setup>
-import RecipeCard from "@/components/RecipeCard/RecipeCard.vue";
+import SliderCard from "@/components/SliderCard/SliderCard.vue";
+import { Carousel, Slide, Pagination, Navigation } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 
 const props = defineProps({
   items: Array,
 });
+
+const settings = {
+  itemsToShow: 1,
+  snapAlign: "center",
+  //  autoplay: 6000,
+};
+
+const breakpoints = {
+  // Mobile
+  600: {
+    itemsToShow: 1,
+    snapAlign: "center",
+  },
+  // Tablet
+  768: {
+    itemsToShow: 1.5,
+    snapAlign: "center",
+  },
+  // Tablet Wide
+  900: {
+    itemsToShow: 2,
+    snapAlign: "center",
+  },
+  // Desktop
+  1024: {
+    itemsToShow: 2.5,
+    snapAlign: "center",
+  },
+  // Widescreen
+  1280: {
+    itemsToShow: 3,
+    snapAlign: "center",
+  },
+  //
+  1600: {
+    itemsToShow: 4,
+    snapAlign: "center",
+  },
+};
 </script>
 
 <style scoped>
-.slider-container {
-  width: 1200px;
-}
-.flicking-panel {
+.slider {
   width: 100%;
+  display: flex;
 }
 </style>
