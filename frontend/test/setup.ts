1fe68699e3820dc60f7fc89df9f1a2ed76668373
Amadeus Figueiredo <amadeusfigue@gmail.com>
2024-06-19 13:34:13 +0200

    test: mock vue3-carousel to prevent testing errors

--- frontend/test/setup.ts
+++ frontend/test/setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
+
+// Mock vue3-carousel to prevent errors by testing
+vi.mock('vue3-carousel', () => ({
+  Carousel: {
+    template: '<div><slot></slot></div>',
+  },
+}))
