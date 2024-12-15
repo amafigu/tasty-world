780bba77c12f34a526f7cb663d35b65a58a07f16
amafigu <amadeusfigue@gmail.com>
2024-12-15 22:54:46 +0100

    add db_host

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -2,13 +2,14 @@ import dotenv from 'dotenv'
 import { Sequelize } from 'sequelize-typescript'
 import Recipe from './recipe'
 dotenv.config()
-const envFile = process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development';
-dotenv.config({ path: envFile });
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+dotenv.config({ path: envFile })
 
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_URL!, {
+  host: process.env.DB_HOST,
   dialect: 'postgres',
-  models: [Recipe]
+  models: [Recipe],
 })
 
 interface Db {
@@ -20,7 +21,7 @@ interface Db {
 const db: Db = {
   Sequelize,
   sequelize,
-  recipe: Recipe
+  recipe: Recipe,
 }
 
 export default db
