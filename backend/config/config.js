70425d026eb28744d1b89e68f9bd9ea314be30b0
amafigu <amadeusfigue@gmail.com>
2024-12-15 22:53:50 +0100

    use js instead of json

---
+++ backend/config/config.js
@@ -0,0 +1,31 @@
+const dotenv = require('dotenv')
+
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+dotenv.config({ path: envFile })
+
+module.exports = {
+  development: {
+    url: String(process.env.DB_URL),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+}
