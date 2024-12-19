f2f3dca205faeb236a86ee92d64ac442c8405146
amafigu <amadeusfigue@gmail.com>
2024-12-19 22:55:24 +0100

    use dynamic routes

--- frontend/src/constants/routes.ts
+++ frontend/src/constants/routes.ts
@@ -1,5 +1,3 @@
 export const ABOUT: string = '/about'
-export const FISH_RECIPES: string = '/fish-recipes'
 export const HOME: string = '/'
-export const MEAT_RECIPES: string = '/meat-recipes'
-export const VEGETARIAN_RECIPES: string = '/vegetarian-recipes'
+export const RECIPES: string = '/recipes'
