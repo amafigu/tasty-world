fb5f6a76fc0346497ef0f0788ad21741cbb3990f
amafigu <amadeusfigue@gmail.com>
2024-12-19 22:54:03 +0100

    add recipe category seeder

---
+++ backend/src/seeders/recipeCategoriesSeeder.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+import db from '../models/index'
+
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
+
+dotenv.config({ path: envFile })
+
+const recipeCategories = [
+  { id: 1, name: 'meat' },
+  { id: 2, name: 'fish' },
+  { id: 3, name: 'poultry' },
+  { id: 4, name: 'vegetarian' },
+  { id: 5, name: 'vegan' },
+  { id: 6, name: 'dessert' },
+  { id: 7, name: 'italian' },
+  { id: 8, name: 'asian' },
+]
+
+async function seed() {
+  try {
+    for (const recipeCategory of recipeCategories) {
+      const repeatedId = await db.recipeCategories.findByPk(recipeCategory.id)
+      const repeatedCategoryName = await db.recipeCategories.findOne({
+        where: { name: recipeCategory.name },
+      })
+      if (repeatedId || repeatedCategoryName) {
+        console.log(
+          `Category with id:${recipeCategory.id} or name:${recipeCategory.name} already exists`
+        )
+        continue
+      } else {
+        await db.recipeCategories.create(recipeCategory)
+        console.log(`Category ${recipeCategory.name} created`)
+      }
+    }
+    process.exit(0)
+  } catch (error) {
+    console.error('Error by seeding product categories: ', error)
+    process.exit(1)
+  }
+}
+
+seed()
--- backend/src/seeders/recipesSeeder.ts
+++ backend/src/seeders/recipesSeeder.ts
@@ -1,83 +1,153 @@
 import dotenv from 'dotenv'
-const envFile =
-  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
-dotenv.config({ path: envFile })
-
 import db from '../models/index'
 
-const recipes = 
-    [
-        {
-            id: 1,
-            image: "meat.png",
-            name: 'Barbecue with meat & veggies',
-            title: 'For a sommer day with friends.',
-            category: 'meat',
-            time: 30,
-            ingredients: 'meat, vegetables ... table ingredients',
-            instructions: 'first: make fire, second: prepare the meat and the salad'
-        },{
-            id: 2,
-            image: "pizza.png",
-            name: 'Pizza bianca from toscana',
-            title: 'Tradicional italian recipe',
-            category: 'vegeratian',
-            time: 60,
-            ingredients: 'flour, tomato souce ... ingredients',
-            instructions: 'first: make dough, second: prepare the souce'
-        },
-        {
-            id: 3,
-            image: "bowl.png",
-            name: 'Asia express bowl',
-            title: 'Healthy tasty and easy to prepare',
-            category: 'vegeratian',
-            time: 30,
-            ingredients: 'Eggs, tomato, salad, corn ... ingredients',
-            instructions: 'first: slice the ingredients, second: prepare the peanuts souce'
-        },
-        {
-            id: 4,
-            image: "fish.png",
-            name: 'Special spyci salmon',
-            title: 'Fresh, good looking and delicious',
-            category: 'fish',
-            time: 40,
-            ingredients: 'Fish, tomato, salad, corn ... ingredients',
-            instructions: 'first: slice the fish, second: prepare the souce'
-        },
-        {
-            id: 5,
-            image: "meatball.png",
-            name: 'Grandma meat balls',
-            title: 'Impress your friend with this tradicional plate',
-            category: 'meat',
-            time: 20,
-            ingredients: 'Meat, ruccola, salad ... ingredients',
-            instructions: 'first: slice the ingredients, second: prepare the peanuts souce'
-        },
-        {
-            id: 6,
-            image: "burger.png",
-            name: 'Veggie Burger Queen',
-            title: 'Make your vegetarian frieds happy.',
-            category: 'vegeratian',
-            time: 30,
-            ingredients: 'Eggs, tomato, salad, corn ... ingredients',
-            instructions: 'first: slice the ingredients, second: prepare the peanuts souce'
-        }
-    ]
+const envFile =
+  process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development'
 
+dotenv.config({ path: envFile })
+
+const recipes = [
+  {
+    image: 'meat.png',
+    name: 'Barbecue with meat & veggies',
+    title: 'For a sommer day with friends.',
+    categories: [{ id: 1, name: 'meat' }],
+    time: 30,
+    ingredients: 'meat, vegetables ... table ingredients',
+    instructions: 'first: make fire, second: prepare the meat and the salad',
+  },
+  {
+    image: 'pizza.png',
+    name: 'Pizza bianca from toscana',
+    title: 'Tradicional italian recipe',
+    categories: [
+      { id: 4, name: 'vegetarian' },
+      { id: 7, name: 'italian' },
+    ],
+    time: 60,
+    ingredients: 'flour, tomato souce ... ingredients',
+    instructions: 'first: make dough, second: prepare the souce',
+  },
+  {
+    image: 'bowl.png',
+    name: 'Asia express bowl',
+    title: 'Healthy tasty and easy to prepare',
+    categories: [
+      { id: 4, name: 'vegetarian' },
+      { id: 8, name: 'asian' },
+    ],
+    time: 30,
+    ingredients: 'Eggs, tomato, salad, corn ... ingredients',
+    instructions:
+      'first: slice the ingredients, second: prepare the peanuts souce',
+  },
+  {
+    image: 'fish.png',
+    name: 'Special spyci salmon',
+    title: 'Fresh, good looking and delicious',
+    categories: [{ id: 2, name: 'fish' }],
+    time: 40,
+    ingredients: 'Fish, tomato, salad, corn ... ingredients',
+    instructions: 'first: slice the fish, second: prepare the souce',
+  },
+  {
+    image: 'meatball.png',
+    name: 'Grandma meat balls',
+    title: 'Impress your friend with this tradicional plate',
+    categories: [{ id: 1, name: 'meat' }],
+    time: 20,
+    ingredients: 'Meat, ruccola, salad ... ingredients',
+    instructions:
+      'first: slice the ingredients, second: prepare the peanuts souce',
+  },
+  {
+    image: 'burger.png',
+    name: 'Vegan Burger Queen',
+    title: 'Make your vegan frieds happy.',
+    categories: [{ id: 5, name: 'vegan' }],
+    time: 30,
+    ingredients: 'Tomato, salad, corn ... ingredients',
+    instructions:
+      'first: slice the ingredients, second: prepare the peanuts souce',
+  },
+  {
+    image: 'meat.png',
+    name: 'Barbecue with meat & veggies',
+    title: 'For a summer day with friends.',
+    categories: [{ id: 1, name: 'meat' }],
+    time: 30,
+    ingredients: 'meat, vegetables ... table ingredients',
+    instructions: 'first: make fire, second: prepare the meat and the salad',
+  },
+  {
+    image: 'fish.png',
+    name: 'Special spicy salmon',
+    title: 'Fresh, good looking and delicious',
+    categories: [{ id: 2, name: 'fish' }],
+    time: 40,
+    ingredients: 'Fish, tomato, salad, corn ... ingredients',
+    instructions: 'first: slice the fish, second: prepare the souce',
+  },
+  {
+    image: 'chicken.png',
+    name: 'Lemon garlic chicken',
+    title: 'Zesty and flavorful',
+    categories: [{ id: 3, name: 'poultry' }],
+    time: 35,
+    ingredients: 'Chicken, lemon, garlic, herbs ...',
+    instructions: 'Marinate the chicken, then grill or bake to perfection',
+  },
+  {
+    image: 'salad.png',
+    name: 'Mediterranean Quinoa Salad',
+    title: 'Healthy, tasty, and easy to prepare',
+    categories: [
+      { id: 4, name: 'vegetarian' },
+      { id: 5, name: 'vegan' },
+    ],
+    time: 20,
+    ingredients: 'Quinoa, tomato, cucumber, olives ...',
+    instructions: 'Cook quinoa, chop veggies, mix with dressing',
+  },
+  {
+    image: 'cheesecake.png',
+    name: 'New York Cheesecake',
+    title: 'Creamy and timeless classic',
+    categories: [{ id: 6, name: 'dessert' }],
+    time: 90,
+    ingredients: 'Cream cheese, sugar, eggs, graham crackers ...',
+    instructions: 'Prepare crust, whip filling, bake in a water bath',
+  },
+]
 
 async function seed() {
   try {
-    for (const recipe of recipes) {
-      await db.recipe.create(recipe)
+    for (const recipeData of recipes) {
+      // Because of the many to many is needed to first extract categories, create and then set
+      const { categories, ...otherFields } = recipeData
+      const repeatedRecipeName = await db.recipes.findOne({
+        where: { name: recipeData.name },
+      })
+      if (repeatedRecipeName) {
+        console.log(
+          `Recipe with name: ${recipeData.name} already exists, please choose another name`
+        )
+        continue
+      } else {
+        const newRecipe = await db.recipes.create(otherFields)
+        if (categories && categories.length > 0) {
+          const categoryIds = categories.map((cat) => cat.id)
+          await newRecipe.$set('categories', categoryIds)
+          console.log(
+            `Categories ${categories} are settet to recipe ${newRecipe.name}`
+          )
+        }
+      }
     }
-    console.log('Seeding completed successfully.')
+    console.log('Recipes seeding completed successfully.')
     process.exit(0)
   } catch (err) {
-    console.error('Seeding failed:', err)
+    console.error('Recipes failed:', err)
     process.exit(1)
   }
 }
