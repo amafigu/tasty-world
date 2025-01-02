39df5217d4040ef30f5b19e97279bfbc739c4f4e
amafigu <amadeusfigue@gmail.com>
2025-01-02 20:23:47 +0100

    recipe and recipeCategory mocks

---
+++ frontend/src/__tests__/mocks/data/recipeCategoriesMock.ts
@@ -0,0 +1,10 @@
+export const recipeCategoriesMock = [
+  {
+    id: '1',
+    name: 'meat',
+  },
+  {
+    id: '2',
+    name: 'fish',
+  },
+]
---
+++ frontend/src/__tests__/mocks/data/recipeMock.ts
@@ -0,0 +1,10 @@
+export const recipeMock = {
+  id: '1',
+  image: 'meat.png',
+  name: 'Barbecue with meat & veggies',
+  title: 'For a sommer day with friends.',
+  time: 30,
+  ingredients: 'meat, vegetables ... table ingredients',
+  instructions: 'first: make fire, second: prepare the meat and the salad',
+  categories: [{ id: '1', name: 'meat' }],
+}
