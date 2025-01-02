2cd19dfa831ee8ce89f4d49d2e5effc29fcaaaa1
amafigu <amadeusfigue@gmail.com>
2025-01-02 21:23:34 +0100

    routing test helper

--- frontend/src/__tests__/helpers/routing.ts
+++ frontend/src/__tests__/helpers/routing.ts
@@ -1,33 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home/Home.vue'
-import Meat from '@/views/recipes/Meat/Meat.vue'
-import Fish from '@/views/recipes/Fish/Fish.vue'
-import Vegetarian from '@/views/recipes/Vegetarian/Vegetarian.vue'
+import { ABOUT, HOME, RECIPES } from '@/constants/routes'
 import About from '@/views/About/About.vue'
-import type { Component } from 'vue'
+import Home from '@/views/Home/Home.vue'
+import Recipes from '@/views/Recipes/Recipes.vue'
 import {
-  type RenderResult,
   type RenderOptions,
+  type RenderResult,
   render,
 } from '@testing-library/vue'
-import {
-  ABOUT,
-  FISH_RECIPES,
-  HOME,
-  MEAT_RECIPES,
-  VEGETARIAN_RECIPES,
-} from '@/constants/routes'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import type { Component } from 'vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import { mockApolloClient } from './apolloClient'
 
+// import all routes here as in @/router/router.ts
 export const routes = [
   { path: HOME, component: Home },
-  { path: MEAT_RECIPES, component: Meat },
-  { path: FISH_RECIPES, component: Fish },
-  { path: VEGETARIAN_RECIPES, component: Vegetarian },
+  { path: `${RECIPES}/:category`, component: Recipes },
   { path: ABOUT, component: About },
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createMemoryHistory(),
   routes,
 })
 
@@ -36,10 +29,14 @@ export function renderUsingRouter<T = unknown>(
   component: Component,
   options?: RenderOptions<T>
 ): RenderResult {
+  const apolloClient = mockApolloClient()
   return render(component, {
     ...options,
     global: {
       plugins: [router],
+      provide: {
+        [DefaultApolloClient]: apolloClient,
+      },
     },
   })
 }
