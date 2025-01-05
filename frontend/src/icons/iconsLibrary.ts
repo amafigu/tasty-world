489f5297284633166733d9d703cfad744e9f340e
amafigu <amadeusfigue@gmail.com>
2025-01-05 22:38:37 +0100

    add icons to library

--- frontend/src/icons/iconsLibrary.ts
+++ frontend/src/icons/iconsLibrary.ts
@@ -3,12 +3,13 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import {
-  faUser,
-  faSearch,
   faBars,
+  faClock,
+  faSearch,
   faTimes,
+  faUser,
 } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUser, faBars, faSearch, faTimes)
+library.add(faUser, faBars, faSearch, faTimes, faClock)
 
 export { FontAwesomeIcon }
