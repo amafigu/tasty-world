20d28fdd073af68c1918a2062700937eb934f04a
amafigu <amadeusfigue@gmail.com>
2025-01-05 22:39:09 +0100

    add recipe types

---
+++ backend/src/types/types.ts
@@ -0,0 +1,26 @@
+export interface Ingredient  {
+  subcategory?: string; 
+  quantity: string;
+  name: string;
+}
+
+export interface InstructionStep {
+  step: number;
+  description: string;
+}
+
+interface Category {
+  id: number; name: string
+}
+
+export interface RecipeArgs {
+  image?: string
+  name?: string
+  title?: string
+  categories?: Category[]
+  time?: number
+  ingredients?: Ingredient[]
+  instructions?: InstructionStep[]
+  peopleToServe: number
+  difficultyLevel: number
+}
