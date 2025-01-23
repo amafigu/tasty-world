dbe193eb12421c58ef6ee2c9675bd0e13c3986c6
amafigu <amadeusfigue@gmail.com>
2025-01-23 19:54:51 +0100

    add breadcrumb into recipes view

--- frontend/src/views/Recipes/Recipes.vue
+++ frontend/src/views/Recipes/Recipes.vue
@@ -1,24 +1,26 @@
 <template>
   <section class="recipes-view">
-    <span class="recipes-title">{{ `${pageCategorie} recipes` }}</span>
+    <h1 class="recipes-title">{{ `${pageCategorie} recipes` }}</h1>
+    <Breadcrumb />
     <div v-if="error">
       <span>There is an error fetching recipes</span>
     </div>
     <div v-else-if="loading">Loading recipes</div>
     <div v-else class="cards-container">
-      <div v-for="item in filteredRecipes">
+      <template v-for="item in filteredRecipes">
         <RecipeCard :recipe="item" />
-      </div>
+      </template>
     </div>
   </section>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { computed } from 'vue'
 import { useQuery } from '@vue/apollo-composable'
 import { useRoute } from 'vue-router'
 import { GET_RECIPES } from '@/graphql/queries'
 import RecipeCard from '@/components/RecipeCard/RecipeCard.vue'
+import Breadcrumb from '@/components/Breadcrumb/Breadcrumb.vue'
 
 const { result, loading, error } = useQuery(GET_RECIPES)
 const route = useRoute()
@@ -29,7 +31,7 @@ const filteredRecipes = computed(() => {
   const recipes = result.value?.recipes ?? []
 
   return recipes.filter((recipe) =>
-    recipe.categories.some((cat) => cat.name === categoriesParam)
+    recipe.categories.some((cat) => cat.name === categoriesParam),
   )
 })
 </script>
@@ -39,33 +41,17 @@ const filteredRecipes = computed(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding: var(--spacing-small);
 }
 
 .cards-container {
   display: grid;
-  width: 80%;
+  width: 100%;
   margin: 0 auto;
   gap: 1rem;
-  grid-template-columns: 1fr;
+  grid-template-columns: 1fr 1fr;
 }
 
-@media (min-width: 600px) {
-  .cards-container {
-    grid-template-columns: 1fr 1fr;
-  }
-}
-
-@media (min-width: 900px) {
-  .cards-container {
-    grid-template-columns: 1fr 1fr 1fr;
-  }
-}
-
-@media (min-width: 1200px) {
-  .cards-container {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-}
 .page-container {
   width: 80%;
 }
@@ -74,10 +60,30 @@ const filteredRecipes = computed(() => {
 }
 
 .recipes-title {
-  padding: 2.5rem;
+  font-weight: 800;
   margin-bottom: var(--spacing-regular);
-  font-size: var(--font-size-huge);
-  letter-spacing: 0.2rem;
+  line-height: 1;
+  color: #337179;
+  font-size: 2rem;
   text-transform: uppercase;
+  letter-spacing: 0.02em;
+}
+
+@media only screen and (min-width: 600px) {
+  .cards-container {
+    grid-template-columns: 1fr 1fr;
+  }
+}
+
+@media only screen and (min-width: 900px) {
+  .cards-container {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
+}
+
+@media only screen and (min-width: 1200px) {
+  .cards-container {
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+  }
 }
 </style>
